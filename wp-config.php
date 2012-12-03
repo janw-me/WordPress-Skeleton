d09393d13a170c554f9c2f21eb4af62cff6e9181
janw-oostendorp <janw.oostendorp@gmail.com>
2012-12-03 12:11:16 +0100

    cleaned up wp-config

--- wp-config.php
+++ wp-config.php
@@ -1,51 +1,68 @@
 <?php
+$enviroment = getenv( 'APP_ENV' );
 
-$enviroment = getenv('APP_ENV');
-
-if ($enviroment == 'development') {
-    define('DB_NAME', '%%');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_HOST', 'localhost');
-    define('WP_SITEURL', '%%');
-    define('WP_HOME', '%%');
-    define('WP_DEBUG', true);
+if ( $enviroment == 'development' ) {
+	// Database
+	define( 'DB_NAME', '%%' );
+	define( 'DB_USER', 'root' );
+	define( 'DB_PASSWORD', '' );
+	// ftp
+	define( 'FTP_USER', '%%' );
+	define( 'FTP_PASS', '%%' );
+	define( 'FTP_HOST', '%%' );
+	define( 'FTP_SSL', false );
+	// Url
+	define( 'DB_HOST', 'localhost' );
+	define( 'WP_HOME', '%%/' );
+	// Debug
+	define( 'WP_DEBUG', true );
 } else { //live
-    define('DB_NAME', '%%');
-    define('DB_USER', '%%');
-    define('DB_PASSWORD', '%%');
-    define('DB_HOST', 'localhost');
-    define('WP_SITEURL', '%%');
-    define('WP_HOME', '%%');
-    define('WP_DEBUG', false);
+	// Database
+	define( 'DB_NAME', '%%' );
+	define( 'DB_USER', '%%' );
+	define( 'DB_PASSWORD', '%%' );
+	// ftp
+	define( 'FTP_USER', '%%' );
+	define( 'FTP_PASS', '%%' );
+	define( 'FTP_HOST', '%%' );
+	define( 'FTP_SSL', false );
+	// Url
+	define( 'DB_HOST', 'localhost' );
+	define( 'WP_HOME', '%%/' );
+	// Debug
+	define( 'WP_DEBUG', false );
 }
-define('FS_METHOD','direct');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
-$table_prefix  = 'wp_'; // please change
-
-//content dir
+// DB
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+$table_prefix = 'wp_'; // please change
+// URL and dirs
+define( 'WP_SITEURL', WP_HOME . 'wp/' );
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+define( 'WP_CONTENT_URL', WP_HOME . '/content' );
 
 // https://api.wordpress.org/secret-key/1.1/salt
-define('AUTH_KEY',         '%%');
-define('SECURE_AUTH_KEY',  '%%');
-define('LOGGED_IN_KEY',    '%%');
-define('NONCE_KEY',        '%%');
-define('AUTH_SALT',        '%%');
-define('SECURE_AUTH_SALT', '%%');
-define('LOGGED_IN_SALT',   '%%');
-define('NONCE_SALT',       '%%');
+define( 'AUTH_KEY', '%%' );
+define( 'SECURE_AUTH_KEY', '%%' );
+define( 'LOGGED_IN_KEY', '%%' );
+define( 'NONCE_KEY', '%%' );
+define( 'AUTH_SALT', '%%' );
+define( 'SECURE_AUTH_SALT', '%%' );
+define( 'LOGGED_IN_SALT', '%%' );
+define( 'NONCE_SALT', '%%' );
 
 //define('WPLANG', 'nl_NL'); // NL
-define('WPLANG', ''); // English
+define( 'WPLANG', '' ); // English
+// Auto save and Revisions
+define( 'AUTOSAVE_INTERVAL', 300 ); // seconds
+define( 'WP_POST_REVISIONS', 10 );
+define( 'DISALLOW_FILE_EDIT', true );
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+if ( !defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
