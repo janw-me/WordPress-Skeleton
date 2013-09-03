d70449dc1888d9f9fff91804d5fe939a558179e2
janw-oostendorp <janw.oostendorp@gmail.com>
2013-09-03 21:00:27 +0200

    switched to diffrent way to include the default themes

---
+++ content/mu-plugins/register_theme_directory.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Plugin Name: Register themes location
+Description: Registers the default location of the wp themes in <code>/wp/wp-content/themes/</code>
+*/
+register_theme_directory( ABSPATH . 'wp-content/themes/' );
