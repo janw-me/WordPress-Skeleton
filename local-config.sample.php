1e1d0d99f8b3991dbb40f1b40d027d53eb56ac9f
Jan Willem Oostendorp <janw.oostendorp@gmail.com>
2014-05-05 14:29:17 +0200

    revisited the way `wp-config` en logging works

---
+++ local-config.sample.php
@@ -0,0 +1,29 @@
+<?php
+//====================================================================
+// Fill in all the needed constants below
+// You may add more to overrule the ones in `wp-config.php`
+//====================================================================
+
+// Database variables
+define( 'DB_NAME',    '%%' );
+define( 'DB_USER',    '%%' );
+define( 'DB_PASSWORD','%%' );
+define( 'DB_HOST',    'localhost' );
+
+// ftp
+define( 'FTP_USER', '%%' );
+define( 'FTP_PASS', '%%' );
+define( 'FTP_HOST', '%%' );
+define( 'FTP_SSL',  false );
+
+// Url
+define( 'WP_HOME', 'http://example.com' ); // No trainling slash
+
+// Unquote for multisites
+//define('DOMAIN_CURRENT_SITE', 'example.com'); // no `http://` 
+
+// show errors
+define( 'WP_DEBUG_DISPLAY', true );
+
+// This is development, don't change
+define( 'WP_DEVELOPMENT', true );
--- wp-config.php
+++ wp-config.php
@@ -1,46 +1,27 @@
 <?php
-// set with: SetEnv APP_ENV development. In a vhost or a higher .htaccess
-$enviroment = getenv( 'APP_ENV' );
-
-if ( $enviroment == 'development' ) {
-	// Database
-	define( 'DB_NAME', '%%' );
-	define( 'DB_USER', 'root' );
-	define( 'DB_PASSWORD', '' );
-	define( 'DB_HOST', 'localhost' );
-	// ftp
-	define( 'FTP_USER', '%%' );
-	define( 'FTP_PASS', '%%' );
-	define( 'FTP_HOST', '%%' );
-	define( 'FTP_SSL', false );
-	// Url
-	define( 'WP_HOME', '%%' ); // example http://example.com  (no trailing slash)
-	// Debug
-	define( 'WP_DEBUG', true );
-} else { //live
-	// Database
-	define( 'DB_NAME', '%%' );
-	define( 'DB_USER', '%%' );
-	define( 'DB_PASSWORD', '%%' );
-	define( 'DB_HOST', 'localhost' );
-	// ftp
-	define( 'FTP_USER', '%%' );
-	define( 'FTP_PASS', '%%' );
-	define( 'FTP_HOST', '%%' );
-	define( 'FTP_SSL', false );
-	// Url
-	define( 'WP_HOME', '%%' ); // example http://example.com  (no trailing slash)
-	// Debug
-	define( 'WP_DEBUG', false );
+// Check if a local development file is defined and include it.
+if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+  include( dirname( __FILE__ ) . '/local-config.php' );
 }
-// DB
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
-$table_prefix = 'wp_'; // please change with 2-3 random characters
-// URL and dirs
-define( 'WP_SITEURL', WP_HOME . '/wp' );
-define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', WP_HOME . '/content' );
+
+//====================================================================
+// Fill in all the needed constants below
+//====================================================================
+
+// Database
+define( 'DB_NAME',    '%%' );
+define( 'DB_USER',    '%%' );
+define( 'DB_PASSWORD','%%' );
+define( 'DB_HOST',    'localhost' );
+// ftp
+define( 'FTP_USER', '%%' );
+define( 'FTP_PASS', '%%' );
+define( 'FTP_HOST', '%%' );
+define( 'FTP_SSL',  false );
+// Url
+define( 'WP_HOME', 'http://example.com' ); // no trailing slash
+
+$table_prefix = 'wp_'; // please change with 2-3 random letters
 
 // https://api.wordpress.org/secret-key/1.1/salt
 define( 'AUTH_KEY', '%%' );
@@ -52,13 +33,49 @@ define( 'SECURE_AUTH_SALT', '%%' );
 define( 'LOGGED_IN_SALT', '%%' );
 define( 'NONCE_SALT', '%%' );
 
+// Default Language
 define( 'WPLANG', 'nl_NL' ); // leave empty for English
-// Auto save and Revisions
-define( 'AUTOSAVE_INTERVAL', 300 ); // seconds
-define( 'WP_POST_REVISIONS', 10 );
-define( 'DISALLOW_FILE_EDIT', true );
 
-/* That's all, stop editing! Happy blogging. */
+// Multisite
+define( 'WP_ALLOW_MULTISITE', false );
+/* // Unquote for multisites
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', 'example.com'); // no `http://` Also set it in the `local-config.php`
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+/**/
+
+//====================================================================
+// That's all, stop editing! Happy blogging.
+//====================================================================
+// Minor tweaks
+define( 'AUTOSAVE_INTERVAL', 300 ); // autosave every 300 seconds
+define( 'WP_POST_REVISIONS', 10 ); // 10 post revisions
+define( 'DISALLOW_FILE_EDIT', true ); // don't allow to edit files in the wp-admin
+
+// URL and dirs
+define( 'WP_SITEURL', WP_HOME . '/wp' );
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_URL', WP_HOME . '/content' );
+
+// Debug
+define( 'WP_DEBUG', true );
+
+// log errors when it's not a development server
+if ( !defined('WP_DEVELOPMENT') || WP_DEVELOPMENT !== true || WP_DEBUG_DISPLAY === false ) {
+    define( 'WP_DEBUG_LOG', true );
+    define( 'WP_DEBUG_DISPLAY', false );
+    @ini_set( 'display_errors', 0 );
+    @ini_set('log_errors', 1);
+    @ini_set('error_log', WP_CONTENT_DIR . '/debug.log');
+}
+
+// DB
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
 
 /** Absolute path to the WordPress directory. */
 if ( !defined( 'ABSPATH' ) )
